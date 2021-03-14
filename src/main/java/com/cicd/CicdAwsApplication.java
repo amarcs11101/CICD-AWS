9a7f923cb60fd13b26bb4aed5e5169f271a01fd4
amarcs721992@gmail.com <amarcs721992@gmail.com>
2021-03-15 01:32:07 +0530

    CICD with aws and spring boot

---
+++ src/main/java/com/cicd/CicdAwsApplication.java
@@ -0,0 +1,22 @@
+package com.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdAwsApplication {
+	
+	@GetMapping("/")
+	String home() {
+		System.out.println("Hii its demo !!!!");
+		return "Spring is here!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAwsApplication.class, args);
+	}
+
+}
